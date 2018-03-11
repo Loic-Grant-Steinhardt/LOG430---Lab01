f3c9e25ca0c69445b4e4aed93b307f9e6a52e394
Loic-Grant-Steinhardt <grant-steinhardt.loic@hotmail.fr>
2018-03-11 14:04:03 -0400

    Push final labo03.

--- src/main/java/loanutils/TestLoanControler.java
+++ src/main/java/loanutils/TestLoanControler.java
@@ -13,22 +13,25 @@ import mockit.*;
  */
 
 public class TestLoanControler {
-    @Mocked
-    EventBusLocal mockedEventBus;
-    @Mocked
-    CalcLoanItem mockedCalcLoanItem;
-
     LoanItem testLoanItem=new LoanItem();
     LoanControler testLoanControler=new LoanControler();
-    Float pAmo=new Float(1), pTau=new Float(2), pMon=new Float(3), pTim=new Float(4);
+    Float pAmo=new Float(1200), pTau=new Float(1200), pMon=new Float(1200), pTim=new Float(1200);
+
+    @Injectable
+    EventBusLocal mockedEventBus;
+    @Injectable
+    CalcLoanItem mockedCalcLoanItem;
+
 
     @Test
-    public void doOperationAbc() {
+    public void testMensualite() {
+        testLoanItem.setInsurance(pTau);
+        testLoanItem.setLoanType(LoanItem.LoanType.MENSUALITE);
         testLoanControler.setItem(testLoanItem);
         testLoanControler.setLoanType(testLoanItem.getLoanType());
         testLoanControler.updateEntry(pAmo,pTau,0F,pTim);
         new Expectations() {{
-            mockedCalcLoanItem.computeMensAss(testLoanItem); result = anyDouble;
+           mockedCalcLoanItem.computeMensAss(testLoanItem); result = anyDouble;
         }};
 
         new Verifications() {{
@@ -36,6 +39,63 @@ public class TestLoanControler {
             mockedCalcLoanItem.computeDuration(testLoanItem); times = 0;
             mockedCalcLoanItem.computeAmount(testLoanItem); times = 0;
             mockedCalcLoanItem.computeRate(testLoanItem); times = 0;
+        }};
+    }
+
+    @Test
+    public void testDuree() {
+        testLoanItem.setInsurance(pTau);
+        testLoanItem.setLoanType(LoanItem.LoanType.DUREE);
+        testLoanControler.setItem(testLoanItem);
+        testLoanControler.setLoanType(testLoanItem.getLoanType());
+        testLoanControler.updateEntry(pAmo,pTau,pMon,0F);
+        new Expectations() {{
+            mockedCalcLoanItem.computeDuration(testLoanItem); result = anyDouble;
+        }};
+
+        new Verifications() {{
+            mockedCalcLoanItem.computeMensAss(testLoanItem); times = 0;
+            mockedCalcLoanItem.computeDuration(testLoanItem); times = 1;
+            mockedCalcLoanItem.computeAmount(testLoanItem); times = 0;
+            mockedCalcLoanItem.computeRate(testLoanItem); times = 0;
+        }};
+    }
+
+    @Test
+    public void testMontant() {
+        testLoanItem.setInsurance(pTau);
+        testLoanItem.setLoanType(LoanItem.LoanType.MONTANT);
+        testLoanControler.setItem(testLoanItem);
+        testLoanControler.setLoanType(testLoanItem.getLoanType());
+        testLoanControler.updateEntry(0F,pTau,pMon,pTim);
+        new Expectations() {{
+            mockedCalcLoanItem.computeAmount(testLoanItem); result = anyDouble;
+        }};
+
+        new Verifications() {{
+            mockedCalcLoanItem.computeMensAss(testLoanItem); times = 0;
+            mockedCalcLoanItem.computeDuration(testLoanItem); times = 0;
+            mockedCalcLoanItem.computeAmount(testLoanItem); times = 1;
+            mockedCalcLoanItem.computeRate(testLoanItem); times = 0;
+        }};
+    }
+
+    @Test
+    public void testTaux() {
+        testLoanItem.setInsurance(pTau);
+        testLoanItem.setLoanType(LoanItem.LoanType.TAUX);
+        testLoanControler.setItem(testLoanItem);
+        testLoanControler.setLoanType(testLoanItem.getLoanType());
+        testLoanControler.updateEntry(pAmo,0F,pMon,pTim);
+        new Expectations() {{
+            mockedCalcLoanItem.computeRate(testLoanItem); result = anyDouble;
+        }};
+
+        new Verifications() {{
+            mockedCalcLoanItem.computeMensAss(testLoanItem); times = 0;
+            mockedCalcLoanItem.computeDuration(testLoanItem); times = 0;
+            mockedCalcLoanItem.computeAmount(testLoanItem); times = 0;
+            mockedCalcLoanItem.computeRate(testLoanItem); times = 1;
         }};
     }
 }
